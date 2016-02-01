aedea6ef30bf60eb52620b152bf78b4dd531a0e0
COUE <florent.coue@atos.net>
2016-02-01 10:52:44 +0100

    TICKETING-51, TICKETING-52, TICKETING-53 : init sql

--- src/sql/plugins/workflow/plugin/init_db_workflow_data_sample_ticketing.sql
+++ src/sql/plugins/workflow/plugin/init_db_workflow_data_sample_ticketing.sql
@@ -10,12 +10,20 @@ INSERT INTO workflow_action (id_action, name, description, id_workflow, id_state
 	VALUES 	(301,'Initialisation','Initialisation de la sollicitation',301,301,302,1,1,0,1,0),
 			(302,'Qualifier','Qualifier',301,302,303,1,0,0,2,0),
 			(303,'Requalifier','Requalifier',301,303,303,1,0,0,3,0),
-			(304,'Escalader','Escalader',301,303,303,1,0,0,4,0);
-
-
+			(304,'Escalader','Escalader',301,303,303,1,0,0,4,0),
+            (305,'Assigner une sollicitation à une autre entité','Assigner une sollicitation à une autre entité',301,303,303,1,0,0,5,0),
+            (306,'Assigner une sollicitation à un autre agent','Assigner une sollicitation à un autre agent',301,303,303,1,0,0,6,0),
+            (307,'M\'assigner une sollicitation','M\'assigner une sollicitation',301,303,303,1,0,0,7,0);
+			
 INSERT INTO workflow_task (id_task, task_type_key, id_action, display_order) 
 	VALUES 	(301,'taskTicketingGenerateTicketReference',301,1),
 			(302,'taskTicketingAssignUnitLinkedToCategory',301,2),
-			(303,'taskTicketingQualifyTicket',302,1),
-			(304,'taskTicketingQualifyTicket',303,1),
+            (321,'taskTicketingQualifyTicket',302,1),
+            (322,'taskTicketingAssignTicketToUnit',302,2),
+            (341,'taskTicketingQualifyTicket',303,1),
+            (351,'taskTicketingAssignTicketToUnit',305,1),
+            (361,'taskTicketingAssignTicketToUser',306,1),
+            (371,'taskTicketingAssignTicketToMe',307,1),
 			(381,'taskTicketingAssignUpTicket',304,1);
+
+			
