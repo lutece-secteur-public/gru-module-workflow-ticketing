03251497a0cde7b222dca594e404aa5d4f83525e
fgandemer <fgandemer@users.noreply.github.com>
2016-04-25 12:29:24 +0200

    WFTCKETING-10  add 'Assign up level3' state, and split 'respond to info' into 2 actions

--- src/sql/plugins/workflow/plugin/init_db_workflow_data_sample_ticketing.sql
+++ src/sql/plugins/workflow/plugin/init_db_workflow_data_sample_ticketing.sql
@@ -9,19 +9,26 @@ INSERT INTO workflow_state (id_state, name, description, id_workflow, is_initial
 	VALUES	(301,'Nouveau','Nouveau',301,1,0,NULL,1),
 			(303,'A traiter','A traiter',301,0,0,NULL,2),
 			(304,'En attente de compléments par l''usager','En attente de compléments par l''usager',301,0,0,NULL,3),
-			(305,'Traité','Traité',301,0,0,NULL,4);
+			(305,'Escaladé niveau 3','Le ticket est escaladé vers un agent de niveau 3',301,0,0,NULL,4),
+			(306,'Traité','Traité',301,0,0,NULL,5);
 
 INSERT INTO workflow_action (id_action, name, description, id_workflow, id_state_before, id_state_after, id_icon, is_automatic, is_mass_action, display_order, is_automatic_reflexive_action) 
 	VALUES 	(301,'Initialisation','Initialisation de la sollicitation',301,301,303,1,1,0,1,0),
-			(303,'Requalifier','Requalifier la sollicitation',301,303,303,1,0,0,2,0),
-            (304,'Escalader','Escalader',301,303,303,1,0,0,6,0),
-			(305,'Assigner à une autre entité','Assigner la sollicitation à une autre entité',301,303,303,1,0,0,5,0),
-            (306,'Assigner à un autre agent','Assigner la sollicitation à un autre agent',301,303,303,1,0,0,4,0),
-			(307,'Me l''assigner','M''assigner la sollicitation',301,303,303,1,0,0,3,0),
-			(308,'Demander compléments','Demander des informations complémentaires à l''usager',301,303,304,1,0,0,7,0),
-			(309,'Répondre pour l''usager','Répondre à la place de l''usager',301,304,303,1,0,0,8,0),
-            (310,'Répondre à l''usager','Répondre à l''usager',301,303,305,1,0,0,9,0),
-            (311, 'Répondre à l''escalade', 'Répondre à l''escalade', 301, 303, 303, 1, 0, 0, 11, 0);
+			(303,'Requalifier','Requalification de la sollicitation',301,303,303,1,0,0,2,0),
+            (304,'Escalader','Escalade vers un agent de niveau 2',301,303,303,1,0,0,6,0),
+            (305,'Escalader','Escalade vers un agent de niveau 3',301,303,305,1,0,0,7,0),
+			(306,'Assigner à une autre entité','Assignation de la sollicitation à une autre entité',301,303,303,1,0,0,5,0),
+            (307,'Assigner à un autre agent','Assignation de la sollicitation à un autre agent',301,303,303,1,0,0,4,0),
+			(308,'Me l''assigner','Prise en charge de la sollicitation',301,303,303,1,0,0,3,0),
+			(309,'Demander compléments','Demande d''informations complémentaires à l''usager',301,303,304,1,0,0,8,0),
+			(310,'Répondre pour l''usager','Réponse à la place de l''usager',301,304,303,1,0,0,9,0),
+			(311,'Répondre (usager)','Réponse de l''usager à une demande d''informations',301,304,303,1,0,0,10,0),
+            (312,'Répondre à l''usager','Réponse finale à l''usager',301,303,306,1,0,0,11,0),
+            (313,'Répondre à l''escalade', 'Réponse à l''escalade', 301, 305, 303, 1, 0, 0, 12, 0),
+   			(314,'Assigner à une autre entité','Assignation de la sollicitation à une autre entité',301,305,305,1,0,0,5,0), -- assignation a autre entite pour tickets escaladés
+            (315,'Assigner à un autre agent','Assignation de la sollicitation à un autre agent',301,305,305,1,0,0,4,0), -- assignation a autre agent pour tickets escaladés
+			(316,'Me l''assigner','Prise en charge de la sollicitation',301,305,305,1,0,0,3,0) -- auto assignation pour tickets escaladés
+;
 		
 INSERT INTO workflow_task (id_task, task_type_key, id_action, display_order) 
 	VALUES 	(301,'taskTicketingGenerateTicketReference',301,1), -- Initialize
@@ -32,27 +39,38 @@ INSERT INTO workflow_task (id_task, task_type_key, id_action, display_order)
             (342,'taskTicketingModifyTicketCategory',303,2),
             (343, 'taskTypeComment', 303,3),
             (344, 'taskTicketingIndexTicket',303,4),
-            (351,'taskTicketingAssignTicketToUnit',305,1), -- Assign to unit
-            (352, 'taskTypeComment', 305,2),
-            (361,'taskTicketingAssignTicketToUser',306,1), -- Assign to user
-            (362, 'taskTypeComment', 306,2),
-            (371,'taskTicketingAssignTicketToMe',307,1), -- Assign to me
-            (372, 'taskTypeComment', 307,2),
+            (351,'taskTicketingAssignTicketToUnit',306,1), -- Assign to unit
+            (352, 'taskTypeComment', 306,2),
+            (361,'taskTicketingAssignTicketToUser',307,1), -- Assign to user
+            (362, 'taskTypeComment', 307,2),
+            (371,'taskTicketingAssignTicketToMe',308,1), -- Assign to me
+            (372, 'taskTypeComment', 308,2),
 			(381,'taskTicketingAssignUpTicket',304,1), -- Assign up
 			(382, 'taskTypeComment', 304,2),
-            (390, 'taskTicketingEditTicket', 308,1), -- Ask for user information
-            (391, 'taskTicketingIndexTicket',308,2),
-            (400, 'taskTicketingEditTicket', 309,1), -- Reply to agent
-            (401, 'taskTicketingSelectChannel', 309,2),
-            (402, 'taskTicketingIndexTicket', 309,3),
-			(420, 'taskTicketingReply', 310,1), -- Reply to user
-			(421, 'taskTicketingIndexTicket', 310,2),
-            (441, 'taskTicketingReplyAssignUpTicket',311,1), -- Reply to assign up
-            (442, 'taskTypeComment', 311,2)
+			(386,'taskTicketingAssignUpTicket',305,1), -- Assign up level 3
+			(387, 'taskTypeComment', 305,2),
+            (390, 'taskTicketingEditTicket', 309,1), -- Ask for user information
+            (391, 'taskTicketingIndexTicket',309,2),
+            (400, 'taskTicketingEditTicket', 310,1), -- Reply to info request from agent to agent
+            (401, 'taskTicketingSelectChannel', 310,2),
+            (403, 'taskTicketingIndexTicket', 310,4),
+            (410, 'taskTicketingEditTicket',311,1), -- Reply to info request from user to agent
+            (411, 'taskTicketingSelectChannel',311,2),
+            (413, 'taskTicketingIndexTicket', 311,4),
+			(420, 'taskTicketingReply', 312,1), -- Reply to user
+            (423, 'taskTicketingIndexTicket', 312,3),
+            (441, 'taskTicketingReplyAssignUpTicket',313,1), -- Reply to assign up
+            (442, 'taskTypeComment', 313,2),
+            (451,'taskTicketingAssignTicketToUnit',314,1), -- Assign to unit LEVEL3
+            (452, 'taskTypeComment', 314,2),
+            (461,'taskTicketingAssignTicketToUser',315,1), -- Assign to user LEVEL3
+            (462, 'taskTypeComment', 315,2),
+            (471,'taskTicketingAssignTicketToMe',316,1), -- Assign to me LEVEL3
+            (472, 'taskTypeComment', 316,2)
 ;
 
 
-DELETE FROM workflow_task_comment_config WHERE id_task >= 300 AND id_task < 450;			
+DELETE FROM workflow_task_comment_config WHERE id_task >= 300 AND id_task < 500;			
 INSERT INTO workflow_task_comment_config (id_task, title, is_mandatory) 
 	VALUES	(325, 'Commentaire', 0),
 			(343, 'Commentaire', 0),
@@ -60,14 +78,20 @@ INSERT INTO workflow_task_comment_config (id_task, title, is_mandatory)
 			(362, 'Commentaire', 0),
 			(372, 'Commentaire', 0),
 			(382, 'Commentaire', 0),
-            (442, 'Commentaire', 0)
+			(387, 'Commentaire', 0),
+            (442, 'Commentaire', 0),
+            (452, 'Commentaire', 0),
+            (462, 'Commentaire', 0),
+			(472, 'Commentaire', 0)
 ;
 
 DELETE FROM workflow_task_ticketing_edit_ticket_config;
 INSERT INTO workflow_task_ticketing_edit_ticket_config (id_task, message_direction, id_user_edition_action) 
-    VALUES  (390, 1, 309),  -- Ask for user information
-            (400, 0, 309) -- Reply to agent
+    VALUES  (390, 1, 311),  -- Ask for user information
+            (400, 0, 310), -- Reply to agent from agent
+            (410, 0, 311) -- Reply to agent from user
 ;
+			
 			
 DELETE FROM workflow_task_ticketing_reply_config;
 INSERT INTO workflow_task_ticketing_reply_config (id_task, message_direction) 
