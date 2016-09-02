3115d735823a4673179e940a61720fd97b620463
MedAmiri <mohammed.amiri@atos.net>
2016-09-02 17:35:15 +0200

    WFTCKETING-19 notification in the case of no nomenclature modification

--- src/java/fr/paris/lutece/plugins/workflow/modules/ticketing/service/task/TaskModifyTicketNomenclature.java
+++ src/java/fr/paris/lutece/plugins/workflow/modules/ticketing/service/task/TaskModifyTicketNomenclature.java
@@ -78,6 +78,12 @@ public class TaskModifyTicketNomenclature extends AbstractTicketingTask
         if ( ticket != null )
         {
             strOldNomenclature = ticket.getNomenclature(  );
+            
+            if( strOldNomenclature == null )
+            {
+            	strOldNomenclature = StringUtils.EMPTY;
+            }
+            
             strNewNomenclature = request.getParameter( PARAMETER_TICKET_NOMENCLATURE );
 
             if ( !strNewNomenclature.equals( strOldNomenclature ) )
