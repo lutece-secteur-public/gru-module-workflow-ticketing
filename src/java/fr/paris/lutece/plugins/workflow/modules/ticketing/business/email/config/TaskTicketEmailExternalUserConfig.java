70075ede3efcc6fef57e414131fbf3421817c568
pplouy <paul.plouy@atos.net>
2017-05-03 15:02:41 +0200

    WFTCKETING-40 : confign for task Email ext user - fields not mandatory

--- src/java/fr/paris/lutece/plugins/workflow/modules/ticketing/business/email/config/TaskTicketEmailExternalUserConfig.java
+++ src/java/fr/paris/lutece/plugins/workflow/modules/ticketing/business/email/config/TaskTicketEmailExternalUserConfig.java
@@ -45,10 +45,8 @@ public class TaskTicketEmailExternalUserConfig extends TaskConfig
     @NotNull
     private MessageDirectionExternalUser _messageDirectionExternalUser;
 
-    @NotNull
     private Integer _nIdFollowingAction;
 
-    @NotNull
     private Integer _nIdContactAttribute;
 
     /**
