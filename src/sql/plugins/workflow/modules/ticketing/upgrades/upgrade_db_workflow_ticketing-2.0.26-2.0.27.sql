b85a8b14d5b96d7ece593f6ba5656b23779ea205
pierre.sciorella <pierre.sciorella@accenture.com>
2019-12-06 14:53:05 +0100

    O2T 78478: param tâche relance auto

--- src/sql/plugins/workflow/modules/ticketing/upgrades/upgrade_db_workflow_ticketing-2.0.26-2.0.27.sql
+++ src/sql/plugins/workflow/modules/ticketing/upgrades/upgrade_db_workflow_ticketing-2.0.26-2.0.27.sql
@@ -16,14 +16,25 @@ INSERT INTO `workflow_action` (`id_action`, `name`, `description`, `id_workflow`
 -- dernière action manuelle = "Solliciter un contributeur" [à partir d\'un niveau 3]
 (344, 'Retour de la sollicitation', 'Retour de la sollicitation après relances automatiques infructueuses', 301, 307, 305, 1, 0, 0, 42, 0);
 
-INSERT INTO `workflow_task` (`id_task`, `task_type_key`, `id_action`, `display_order`) VALUES
-(569, 'taskNotifyWaitingTicket', 340, 1);
-
 
 -- paramétrage de la tâche de relance automatique
+INSERT INTO `workflow_task` (`id_task`, `task_type_key`, `id_action`, `display_order`) VALUES
+(569, 'taskNotifyWaitingTicket', 340, 1),
+(570, 'taskNotifyGru',340, 2);
 
--- (569, 'Ville de Paris', '[GRU] Relance n°${nb_automatic_notification!} - Le service ${unit_name!} requiert une action de votre part sur la sollicitation ${reference!}', '<p>Relance n&deg;${nb_automatic_notification!} :</p><br><p>Ceci est une relance qui vous est adress&eacute;e par le service ${unit_name!} concernant la demande li&eacute;e &agrave; la sollicitation ${reference!}. <a href="${ticketing_ticket_link!}">Cliquez ici</a> pour y r&eacute;pondre (attention : apr&egrave;s validation, vous ne pourrez plus modifier votre r&eacute;ponse ni ajouter de compl&eacute;ment d\'information).</p>');
-
-
+INSERT INTO test.workflow_task_notify_gru_cf (id_task, id_spring_provider, marker_provider_ids, demand_status, crm_status_id, set_onglet, message_guichet, status_text_guichet,
+                                              sender_name_guichet, subject_guichet, demand_max_step_guichet, demand_user_current_step_guichet, is_active_onglet_guichet, status_text_agent,
+                                              message_agent, is_active_onglet_agent, subject_email, message_email, sender_name_email, recipients_cc_email, recipients_cci_email,
+                                              is_active_onglet_email, message_sms, billing_group_sms, billing_account_sms, is_active_onglet_sms, id_mailing_list_broadcast,
+                                              email_broadcast, sender_name_broadcast, subject_broadcast, message_broadcast, recipients_cc_broadcast, recipients_cci_broadcast,
+                                              is_active_onglet_broadcast)
+                                              VALUES
+(570, 'workflow-ticketing.externaluser.provider-manager.@.ticket', '', 0, 0, 4,
+ null, null, null, null, 0, 0, 0,
+ null, null, 0, null, null, null, null, null,
+ 0, null, null, null, 0, -1, '${email_recipients!}',
+ 'Mairie de Paris', '[GRU] Relance n°${nb_automatic_notification!} - Le service ${unit_name!} requiert une action de votre part sur la sollicitation ${reference!}',
+ '<p>Relance n&deg;${nb_automatic_notification!}:</p><p>Ceci est une relance qui vous est adress&eacute;e par le service ${unit_name!} concernant la demande li&eacute;e &agrave; la sollicitation ${reference!}. <a href="${ticketing_ticket_link!}">Cliquez ici</a> pour y r&eacute;pondre (attention : apr&egrave;s validation, vous ne pourrez plus modifier votre r&eacute;ponse ni ajouter de compl&eacute;ment d''information).</p>',
+ '', '', 1);
 
 
