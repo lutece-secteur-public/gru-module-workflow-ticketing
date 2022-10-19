f82b566dff2f3ee6c48036fedde77e65c183c00d
GBLAcc <gaelle.blancho@accenture.com>
2022-10-19 16:11:01 +0200

    GRUTICKET295-Supprimer vsp ou signalé

---
+++ src/java/fr/paris/lutece/plugins/workflow/modules/ticketing/service/task/TaskRemoveSignalementTicket.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2002-2022, City of Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.workflow.modules.ticketing.service.task;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+
+import fr.paris.lutece.plugins.ticketing.business.ticket.Ticket;
+import fr.paris.lutece.plugins.ticketing.business.ticket.TicketHome;
+import fr.paris.lutece.portal.service.i18n.I18nService;
+
+/**
+ * This class represents a task to report (signalement) the ticket
+ *
+ */
+public class TaskRemoveSignalementTicket extends AbstractTicketingTask
+{
+    // Messages
+    private static final String MESSAGE_SIGNALEMENT_TICKET = "module.workflow.ticketing.task_remove_signalement_ticket.labelSignalementTicket";
+
+    @Override
+    public String processTicketingTask( int nIdResourceHistory, HttpServletRequest request, Locale locale )
+    {
+        String strTaskInformation = StringUtils.EMPTY;
+        StringBuilder sb = new StringBuilder( );
+
+        // We get the ticket to report (signalement)
+        Ticket ticket = getTicket( nIdResourceHistory );
+
+        if ( ticket != null )
+        {
+
+            TicketHome.update( ticket );
+
+            strTaskInformation = sb.toString( );
+        }
+
+        return strTaskInformation;
+    }
+
+    @Override
+    public String getTitle( Locale locale )
+    {
+        return I18nService.getLocalizedString( MESSAGE_SIGNALEMENT_TICKET, locale );
+    }
+
+}
---
+++ src/sql/plugins/workflow/modules/ticketing/upgrades/upgrade_db_workflow_ticketing-3.1.1-3.1.2.sql
@@ -0,0 +1,43 @@
+--- Possiblite de supprimer Signalement et Vous simplifier Paris
+
+-- Vider emplacement pour intercaler boutons
+UPDATE workflow_action SET display_order = display_order + 1 WHERE display_order > 5; 
+UPDATE workflow_action SET display_order = display_order + 1 WHERE display_order > 7; 
+UPDATE workflow_action SET display_order = display_order + 1 WHERE display_order > 30; 
+UPDATE workflow_action SET display_order = display_order + 1 WHERE display_order > 32; 
+UPDATE workflow_action SET display_order = display_order + 1 WHERE display_order > 42;
+UPDATE workflow_action SET display_order = display_order + 1 WHERE display_order > 44;
+
+-- Ajout de 2 actions - boutons
+INSERT INTO workflow_action
+(id_action, name, description, id_workflow, id_state_before, id_state_after, id_icon, is_automatic, is_mass_action, display_order, is_automatic_reflexive_action)
+VALUES(358,'Supprimer Signalement', 'Suppression du signalement', 301, 303, 303, 1, 0, 0, 6, 0),
+(359, 'Supprimer Signalement', 'Suppression du signalement', 301, 305, 305, 1, 0, 0, 31, 0),
+(360,'Supprimer Signalement', 'Suppression du signalement', 301, 306, 306, 1, 0, 0, 43, 0),
+(361,'Supprimer Vous simplifier Paris', 'Suppression de l''action Vous simplifier Paris', 301, 303, 303, 1, 0, 0, 8, 0),
+(362,'Supprimer Vous simplifier Paris', 'Suppression de l''action Vous simplifier Paris', 301, 305, 305, 1, 0, 0, 32, 0),
+(363,'Supprimer Vous simplifier Paris', 'Suppression de l''action Vous simplifier Paris', 301, 306, 306, 1, 0, 0, 45, 0);
+
+-- Ajout couleur rouge clair
+INSERT INTO ticketing_couleur_bouton (id_couleur, couleur) VALUES('Rouge clair', '#DE2916');
+
+
+-- Parametrage des 2 boutons           
+INSERT INTO ticketing_param_bouton_action
+(id_action, id_couleur, ordre, icone, id_groupe)
+VALUES(358, 'Rouge clair', 26, 'fa fa-question-circle-o', 2),
+(359, 'Rouge clair', 27, 'fa fa-question-circle-o', 2),
+(360, 'Rouge clair', 28, 'fa fa-question-circle-o', 2),
+(361, 'Rouge clair', 29, 'fa fa-question-circle-o', 2),
+(362, 'Rouge clair', 30, 'fa fa-question-circle-o', 2),
+(363, 'Rouge clair', 31, 'fa fa-question-circle-o', 2);
+
+-- Ajout de 2 taches
+INSERT INTO workflow_task
+(id_task, task_type_key, id_action, display_order)
+VALUES(620,'taskRemoveSignalementTicket', 358, 1),
+(621,'taskRemoveSignalementTicket', 359, 1),
+(622,'taskRemoveSignalementTicket', 360, 1),
+(624, 'taskRemoveVousSimplifierParis', 361, 1),
+(625,'taskRemoveVousSimplifierParis', 362, 1),
+(626,'taskRemoveVousSimplifierParis', 363, 1);
