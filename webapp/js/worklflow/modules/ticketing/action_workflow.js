56c6a4a207369bafbdec1c27c0eb739de5b83a6a
GaelleB <gaelle.blancho@accenture.com>
2025-11-04 16:15:51 +0100

    BC14387-Amelioration reactivation bouton pop-in en cas de réouverture

--- webapp/js/worklflow/modules/ticketing/action_workflow.js
+++ webapp/js/worklflow/modules/ticketing/action_workflow.js
@@ -1,7 +1,26 @@
 $( function() {	
-	$('#next_button').prop('disabled', false);
+	
+	var form = $('#wf_action_form');
+	var boutonSubmit = $('#next_button');
+	
+	reactiverBouton();
 
 	$('#wf_action_form').submit(function( ){
-		$('#next_button').prop('disabled', true);
+		desactiverBouton();
 	});	
+	
+	// Réactivation à chaque ouverture de pop-in Bootstrap
+	$('#ticketing-modal-workflow-action-form').on('show.bs.modal', function () {
+	  console.log('ouverture boo');
+	  reactiverBouton();
+	});
+	
+	function reactiverBouton(){
+		boutonSubmit.prop('disabled', false);
+		form.removeClass('js_erreur_wkf');
+	}
+
+	function desactiverBouton(){
+		boutonSubmit.prop('disabled', true);
+	}
 });
